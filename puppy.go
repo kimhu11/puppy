49c8bff36efdca0e2acef60ad710f5f829a70ea5
unknown <yizhou.hu@hotmail.com>
2023-07-11 23:25:07 -0700

    second commit

--- puppy.go
+++ puppy.go
@@ -2,12 +2,18 @@ package puppy
 
 import (
 	"fmt"
+
+	dog "github.com/kimhu11/dog1"
 )
 
 func Version() {
-	fmt.Println("Version v1.0.0")
+	fmt.Println("Version v1.1.0")
 }
 
 func Bark() string {
 	return "woof woof!"
+}
+
+func DogBark() string {
+	return dog.WhenGrownUp(Bark())
 }
